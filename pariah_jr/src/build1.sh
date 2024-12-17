d19ee580d7c201bdea834da55c129e39aed4f441
Stelios Rotas <steliosrotas@yahoo.com>
2024-12-17 21:41:16 +0200

    added another input

---
+++ pariah_jr/src/build1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -xe
+
+clang -Wall -Wextra -o twice twice.c
+clang -Wall -Wextra -o gates gates.c
---
+++ pariah_jr/src/twice.c
@@ -0,0 +1,7 @@
+/* File: twice.c*/
+
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
