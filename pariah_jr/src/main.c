846d3584d0b5805a8ae9894af1d8e901ac4ff460
Stelios Rotas <steliosrotas@yahoo.com>
2024-12-17 08:56:16 +0200

    small progress

--- pariah_jr/src/main.c
+++ pariah_jr/src/main.c
@@ -18,11 +18,11 @@ float rand_float(void) {
     return (float) rand() / (float)RAND_MAX;
 }
 
-float cost(float w) {
+float cost(float w, float b) {
     float result = 0.0f;
     for (size_t i=0; i < train_count; ++i) {
         float x = train[i][0];
-        float y = x*w;
+        float y = x*w + b;
         float d = y - train[i][1];
         result += d*d;
     }
@@ -30,20 +30,23 @@ float cost(float w) {
 }
 
 int main() {
-    // srand(time(0));
-    srand(69);
+    srand(time(0));
     float w = rand_float()*10.0f;
+    float b = rand_float()*5.0f;
 
     float eps = 1e-3;
     float rate = 1e-3;
 
-    printf("%f\n", cost(w));
+    printf("%f\n", cost(w, b));
     for (size_t i = 0; i < 500; ++i) {
-        float dcost = (cost(w+eps) - cost(w))/eps;
-        w -= rate*dcost;
-        printf("%f\n", cost(w));
+        float c = cost(w, b);
+        float dw = (cost(w+eps, b) - c)/eps;
+        float db = (cost(w, eps + b) - c)/eps;
+        w -= rate*dw;
+        b -= rate*db;
+        printf("cost: %f, w = %f, b = %f\n", cost(w, b), w, b);
     }
     printf("---------------\n");
-    printf("%f\n", w);
+    printf("w = %f, b = %f \n", w, b);
     return 0;
 }
